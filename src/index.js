effea5e95a5031f79faed80e5cd4c8c11589199b
Rahul Singh <rscodedocs@gmail.com>
2020-10-11 00:58:53 +0530

    basic express setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('Hello from socket testing');
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/hello', (_req, res) => res.status(200).send('Ok '));
+
+app.listen(4000, () => console.log(`Running successfully at port 4000`));
