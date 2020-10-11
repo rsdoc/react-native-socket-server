6ab927b2f7f88e858af2f2d8090e903965053ad6
Rahul Singh <rscodedocs@gmail.com>
2020-10-12 02:15:57 +0530

    added socket

--- src/index.js
+++ src/index.js
@@ -1,13 +1,62 @@
+import { createServer } from 'http';
+
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import socketIo from 'socket.io';
 
 const app = express();
 
-app.use(cors());
-app.use(morgan('dev'));
+const server = createServer(app);
+
+const io = socketIo(server);
+
 app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
 
-app.get('/hello', (_req, res) => res.status(200).send('Ok '));
+app.get('/hello', (_req, res) => res.status(200).json({message: "welcome"}));
 
-app.listen(4000, () => console.log(`Running successfully at port 4000`));
+io.on('connection', (socket) => {
+    console.log('new connection added');
+    socket.on('start-timer', () => {
+        timer();
+    });
+
+    function timer() {
+        let start = new Date().getTime();
+        let elapsedTime = '0.0';
+
+        setInterval(() => {
+            let time = new Date().getTime() - start;
+
+            elapsedTime = Math.floor(time / 100) / 10;
+
+            if(Math.round(elapsedTime) === elapsedTime) { elapsedTime += '.0';
+                socket.emit('sync-time', { elapsedTime})
+            }
+
+        }, 100);
+    }
+    //
+    // setInterval(() => {
+    //     socket.emit("intrval", {data: "hello"})
+    // }, 3000);
+
+});
+
+// function timer() {
+//     let start = new Date().getTime();
+//     let elapsedTime = '0.0';
+//
+//     setInterval(() => {
+//         let time = new Date().getTime() - start;
+//
+//         elapsedTime = Math.floor(time / 100) / 10;
+//
+//         if(Math.round(elapsedTime) === elapsedTime) { elapsedTime += '.0'; }
+//
+//     }, 100);
+// }
+
+server.listen(4000, () => console.log(`Running successfully at port 4000`));
